a2e4781996adaa7b79fdd1bce49e1ab6957bad68
kelu241 <kelu241@gmail.com>
2026-03-21 18:17:13 -0300

    feat:crud pronto

--- src/main/java/com/luciano/thymeleaf/controller/ProdutoController.java
+++ src/main/java/com/luciano/thymeleaf/controller/ProdutoController.java
@@ -1,5 +1,6 @@
 package com.luciano.thymeleaf.controller;
 
+import java.util.UUID;
 import com.luciano.thymeleaf.model.Produto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -7,7 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.ModelAndView;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -60,11 +63,35 @@ public class ProdutoController {
 
   @PostMapping("/adicionar")
   public String adicionarProduto(@ModelAttribute Produto produto) {
-
-    if (produto != null)
-      this.produtos.add(produto);
+    produto.setId(UUID.randomUUID().toString());
+    this.produtos.add(produto);
     return "redirect:/produtos";
 
+  }
+
+  @GetMapping("/atualizar/{id}")
+  public ModelAndView atualiza(@PathVariable String id) {
+
+    Produto produto = this.produtos.stream()
+        .filter(p -> p.getId().equals(id))
+        .findFirst()
+        .orElse(null);
+    ModelAndView model = new ModelAndView("/produtos/form_produtos");
+    model.addObject(produto);
+    return model;
+
+  }
+
+  @PutMapping("/edicao/{id}")
+  public String edicao(@PathVariable String id, @ModelAttribute Produto produto) {
+    Produto produto_antigo = this.produtos.stream()
+        .filter(p -> p.getId().equals(id))
+        .findFirst().orElse(null);
+    produto_antigo.setNome(produto.getNome());
+    produto_antigo.setValor(produto.getValor());
+    produto_antigo.setAno(produto.getAno());
+    produto_antigo.setDataLancamento(produto.getDataLancamento());
+    return "redirect:/produtos";
   }
 
 }
--- src/main/java/com/luciano/thymeleaf/model/Produto.java
+++ src/main/java/com/luciano/thymeleaf/model/Produto.java
@@ -6,14 +6,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.UUID;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Produto {
+
   private String id;
   private String nome;
   private Double valor;
   private Integer ano;
+
+  @DateTimeFormat(pattern = "yyyy-MM-dd")
   private LocalDate dataLancamento;
 
   public Produto(String nome, Double valor, Integer ano, LocalDate dataLancamento) {
